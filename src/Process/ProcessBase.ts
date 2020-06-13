f4266f38de254bccddca7ee75ca2e9faa16e1fda
Technote <technote.space@gmail.com>
2020-06-13 17:07:25 +0900

    fix: lint error

--- src/Process/ProcessBase.ts
+++ src/Process/ProcessBase.ts
@@ -2,7 +2,7 @@ import {IProcess} from '..';
 import {DEFAULT_SLEEP} from '../constants';
 
 export abstract class ProcessBase<ObjectType> implements IProcess<ObjectType> {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/explicit-module-boundary-types,@typescript-eslint/no-unused-vars
   public async init(context: any): Promise<void> {
     //
   }
@@ -15,7 +15,7 @@ export abstract class ProcessBase<ObjectType> implements IProcess<ObjectType> {
     return DEFAULT_SLEEP;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/explicit-module-boundary-types
   public abstract reset(context: any): Promise<void>;
 
   public abstract step(): Promise<void>;
